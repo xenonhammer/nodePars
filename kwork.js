d791478a2a80475e1860244b31ba93f0b9b227ba
Arthur Aliev <xenonhammer46@gmail.com>
2020-06-30 22:10:39 +0400

    Настройка парсинга

--- kwork.js
+++ kwork.js
@@ -1,12 +1,11 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const fs = require('fs')
 
-var data = [];
+var parseData = [];
 var i = 0;
 
 function kworkParser(
-    data,
+    parseData,
     url = 'https://kwork.ru/projects?', 
     category = "c=11", 
     pageNum = 0, 
@@ -18,11 +17,12 @@ function kworkParser(
     .then(response => {
         let $ = cheerio.load(response.data);
         $('.card.want-card').each((i, elem) => {
-            data.push(
-            {i = {
+            index++ 
+            parseData.push(
+            {[index] : {
                 'id': Math.floor(Math.random()*999999999),
                 'title': $(elem).find('.wants-card__header-title:first-child').text(),
-                'link': $(elem).find('.wants-card__header-title a').attr('href'),
+                'href': $(elem).find('.wants-card__header-title a').attr('href'),
                 'price': $(elem).find('.wants-card__header-price.wants-card__price.m-hidden').text().replace(/[^0-9](fs12)?/ig, ''),
                 'description': $(elem).find('.first-letter.hidden').text() 
                     ? $(elem).find('.first-letter.hidden').text().replace(/Скрыть/ig, '') 
@@ -34,14 +34,12 @@ function kworkParser(
         let page = `&page=${pageNum}`;
         url = url.replace(/(c\=[0-9]{1,3})?&page=+[0-9]{1,3}/igm, '')
         url = url + category + page
-        i++
-        console.log('data', data)
-        if(pageNum === breakpoint){
-            return JSON.stringify(data);
-        };
 
-        kworkParser(data, url, category, pageNum, breakpoint)
+        if(pageNum === breakpoint){
+            return JSON.stringify(parseData);
+        };
+        kworkParser(parseData, url, category, pageNum, breakpoint)
     }) 
 }
 
-kworkParser(data)
+kworkParser(parseData)
